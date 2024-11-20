b0cda418769b6b4ccbfd077b0047c253ab294655
josevissoti <josevissoti@gmail.com>
2024-11-20 18:14:48 -0300

    criando projeto

---
+++ src/main/java/com/curso/petabyte/PetabyteApplication.java
@@ -0,0 +1,13 @@
+package com.curso.petabyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetabyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetabyteApplication.class, args);
+	}
+
+}
