438d1d217c759c4724312f1c3603727d4fc60045
josevissoti <josevissoti@gmail.com>
2025-03-19 22:08:41 -0300

    Implementando Entidade Pessoa

---
+++ src/main/java/com/curso/domains/Pessoa.java
@@ -0,0 +1,110 @@
+package com.curso.domains;
+
+import com.curso.domains.enums.TipoPessoa;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public abstract class Pessoa {
+    protected Long idPessoa;
+    protected String nome;
+    protected String cpf;
+    protected LocalDate dataNascimento;
+    protected String email;
+    protected String senha;
+    protected LocalDate dataCriacao;
+    protected Set<Integer> tipoPessoa = new HashSet<>();
+
+    public Pessoa() {
+        addTipoPessoa(TipoPessoa.USUARIO);
+    }
+
+    public Pessoa(Long idPessoa, String nome, String cpf, LocalDate dataNascimento, String email, String senha) {
+        this.idPessoa = idPessoa;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.senha = senha;
+        addTipoPessoa(TipoPessoa.USUARIO);
+    }
+
+    public Long getIdPessoa() {
+        return idPessoa;
+    }
+
+    public void setIdPessoa(Long idPessoa) {
+        this.idPessoa = idPessoa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Set<TipoPessoa> getTipoPessoa() {
+        return tipoPessoa.stream().map(TipoPessoa::toEnum).collect(Collectors.toSet());
+    }
+
+    public void addTipoPessoa(TipoPessoa tipoPessoa) {
+        this.tipoPessoa.add(tipoPessoa.getId());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(idPessoa, pessoa.idPessoa) && Objects.equals(cpf, pessoa.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idPessoa, cpf);
+    }
+}
